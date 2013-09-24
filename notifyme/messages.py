fa1460fedfaa92efd52b7f92cb93273776f9dcd3
Johannes Fürmann <johannes@weltraumpflege.org>
2013-09-24 17:45:27 +0200

    add tests and fix messages

--- notifyme/messages.py
+++ notifyme/messages.py
@@ -47,7 +47,17 @@ class PublishMessage(ProtocolMessage):
     """
     def __init__(self, published_resources):
         ProtocolMessage.__init__(self)
+        if type(published_resources) is not list:
+            raise ValueError('published_resources must be a list of str')
+        for element in published_resources:
+            if type(element) is not str:
+                raise ValueError('published_resources must be a list of str')
+
         self.data['published_resources'] = published_resources
+
+    @classmethod
+    def from_dict(cls, message_dict):
+        return cls(message_dict['published_resources'])
 
 
 class SubscribeMessage(ProtocolMessage):
@@ -56,7 +66,17 @@ class SubscribeMessage(ProtocolMessage):
     """
     def __init__(self, subscribed_resources):
         ProtocolMessage.__init__(self)
+        if type(subscribed_resources) is not list:
+            raise ValueError('subscribed_resources must be a list of str')
+        for element in subscribed_resources:
+            if type(element) is not str:
+                raise ValueError('subscribed_resources must be a list of str')
+
         self.data['subscribed_resources'] = subscribed_resources
+
+    @classmethod
+    def from_dict(cls, message_dict):
+        return cls(message_dict['subscribed_resources'])
 
 
 class ConfirmationMessage(ProtocolMessage):
@@ -65,7 +85,17 @@ class ConfirmationMessage(ProtocolMessage):
     """
     def __init__(self, confirmed_resources):
         ProtocolMessage.__init__(self)
+        if type(confirmed_resources) is not list:
+            raise ValueError('confirmed_resources must be a list of str')
+        for element in confirmed_resources:
+            if type(element) is not str:
+                raise ValueError('confirmed_resources must be a list of str')
+
         self.data['confirmed_resources'] = confirmed_resources
+
+    @classmethod
+    def from_dict(cls, message_dict):
+        return cls(message_dict['confirmed_resources'])
 
 
 class ErrorMessage(ProtocolMessage):
@@ -73,7 +103,12 @@ class ErrorMessage(ProtocolMessage):
     Notify peer if any errors occur
     """
     def __init__(self, error_message):
+        ProtocolMessage.__init__(self)
         self.data['error_message'] = error_message
+
+    @classmethod
+    def from_dict(cls, message_dict):
+        return cls(message_dict['error_message'])
 
 
 class NotificationMessage(ProtocolMessage):
@@ -83,6 +118,10 @@ class NotificationMessage(ProtocolMessage):
     def __init__(self, notification):
         ProtocolMessage.__init__(self)
         self.data = notification
+
+    @classmethod
+    def from_dict(cls, message_dict):
+        return NotificationMessage(message_dict)
 
 
 class WrappedProtocolMessage:
@@ -99,11 +138,44 @@ class WrappedProtocolMessage:
         """
         self.data = {
             'message_type': type(message).__name__,
-            'message': message}
+            'message': message.data}
 
-        @property
-        def text(self):
-            """
-            Text representation of the message, ready to be sent over the wire
-            """
-            return json.dumps(self.data)
+    @property
+    def text(self):
+        """
+        Text representation of the message, ready to be sent over the wire
+        """
+        return json.dumps(self.data)
+
+    @classmethod
+    def parse(cls, received_bytes):
+        """
+        Parse received JSON Message
+
+        Args:
+            received_bytes(str): Bytes to parse from the message
+
+        Returns:
+            :class:`notifyme.messages.ProtocolMessage`
+
+        Raises:
+            :class:`ValueError` if the message is not in a valid format.
+            :class:`TypeError` if the message_type is not recognized.
+        """
+        try:
+            message_dict = json.loads(received_bytes)
+        except ValueError:
+            raise ValueError("Error Parsing Message")
+
+        if message_dict['message_type'] == 'PublishMessage':
+            return PublishMessage.from_dict(message_dict['message'])
+        elif message_dict['message_type'] == 'SubscribeMessage':
+            return SubscribeMessage.from_dict(message_dict['message'])
+        elif message_dict['message_type'] == 'ConfirmationMessage':
+            return ConfirmationMessage.from_dict(message_dict['message'])
+        elif message_dict['message_type'] == 'ErrorMessage':
+            return ErrorMessage.from_dict(message_dict['message'])
+        elif message_dict['message_type'] == 'NotificationMessage':
+            return NotificationMessage.from_dict(message_dict['message'])
+        else:
+            raise TypeError("Unsupported message type")
---
+++ tests/test_messages.py
@@ -0,0 +1,77 @@
+from unittest import TestCase
+
+from notifyme.messages import *
+
+
+class TestMessageCreation(TestCase):
+    def test_publish_message(self):
+        p = PublishMessage(published_resources=['/test'])
+        self.assertIsNotNone(p)
+        self.assertRaises(ValueError,
+                          PublishMessage,
+                          {'published_resources': 'asdf'})
+        self.assertRaises(ValueError,
+                          PublishMessage,
+                          {'published_resources': ['asdf', 1234]})
+
+    def test_subscribe_message(self):
+        s = SubscribeMessage(subscribed_resources=['/test'])
+        self.assertIsNotNone(s)
+        self.assertRaises(ValueError,
+                          SubscribeMessage,
+                          {'subscribed_resources': 'asdf'})
+        self.assertRaises(ValueError,
+                          SubscribeMessage,
+                          {'subscribed_resources': ['asdf', 1234]})
+
+    def test_confirmation_message(self):
+        c = ConfirmationMessage(confirmed_resources=['/test'])
+        self.assertIsNotNone(c)
+        self.assertRaises(ValueError,
+                          ConfirmationMessage,
+                          {'confirmed_resources': 'asdf'})
+        self.assertRaises(ValueError,
+                          ConfirmationMessage,
+                          {'confirmed_resources': ['asdf', 1234]})
+
+    def test_error_message(self):
+        e = ErrorMessage(error_message="test")
+        self.assertIsInstance(e, ProtocolMessage)
+        self.assertIsInstance(e, ErrorMessage)
+
+    def test_notification_message(self):
+        n = NotificationMessage(notification="test")
+        self.assertIsInstance(n, ProtocolMessage)
+        self.assertIsInstance(n, NotificationMessage)
+
+
+class TestMessageParsing(TestCase):
+    def test_publish_message(self):
+        p = PublishMessage(published_resources=['/test'])
+        w = WrappedProtocolMessage(message=p)
+        p_new = WrappedProtocolMessage.parse(w.text)
+        self.assertDictEqual(p.data, p_new.data)
+
+    def test_subscribe_message(self):
+        s = SubscribeMessage(subscribed_resources=['/test'])
+        w = WrappedProtocolMessage(message=s)
+        s_new = WrappedProtocolMessage.parse(w.text)
+        self.assertDictEqual(s.data, s_new.data)
+
+    def test_confirmation_message(self):
+        c = ConfirmationMessage(confirmed_resources=['/test'])
+        w = WrappedProtocolMessage(message=c)
+        c_new = WrappedProtocolMessage.parse(w.text)
+        self.assertDictEqual(c.data, c_new.data)
+
+    def test_error_message(self):
+        e = ErrorMessage(error_message="lel")
+        w = WrappedProtocolMessage(message=e)
+        e_new = WrappedProtocolMessage.parse(received_bytes=w.text)
+        self.assertDictEqual(e.data, e_new.data)
+
+    def test_notification_message(self):
+        n = NotificationMessage(notification={})
+        w = WrappedProtocolMessage(message=n)
+        n_new = WrappedProtocolMessage.parse(w.text)
+        self.assertDictEqual(n.data, n_new.data)
