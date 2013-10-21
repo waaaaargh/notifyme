427bd4c9cc6b5230331ecb992e5aba3bff6d60a1
Johannes Fürmann <johannes@weltraumpflege.org>
2013-10-21 16:55:25 +0200

    make subscriber executable

---
+++ bin/notifyme-subscriber.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# (c) Johannes Fürmann <johannes@weltraumpflege.org>
+# http://weltraumpflege.org/~johannes
+# This file is part of notifyme.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+from os.path import abspath, dirname, join, exists
+sys.path.append(abspath(join(dirname(__file__), '..')))
+import logging
+
+from notifyme.subscriber import SimpleSubscriber
+
+from argparse import ArgumentParser
+
+def test_cb(x):
+    print("lel")
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description="Notifyme subscriber component")
+    parser.add_argument('-k', '--key', help="key file",
+                        required=True)
+    parser.add_argument('-c', '--certificate', help="certificate file",
+                        required=True)
+    parser.add_argument('-n', '--hostname', help="publisher hostname",
+                        required=True)
+    parser.add_argument('-p', '--portnumber', help="publisher port",
+                        required=True)
+    parser.add_argument('-r', '--resource', help="subscribe to resource",
+                        required=True, action='append')
+    args = parser.parse_args()
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logging.debug("lel")
+    subscriber = SimpleSubscriber(hostname=args.hostname,
+                                  port=int(args.portnumber),
+                                  certfile=args.certificate,
+                                  keyfile=args.key,
+                                  notification_callback=test_cb,
+                                  subscribed_resources=args.resource)
+    subscriber.run()
